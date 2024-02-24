a119714decb41553b4226b6d4fa84f58ec63428f
cp4reallife@yahoo.com <cp4reallife@yahoo.com>
2024-02-24 20:28:53 +0100

    task complete without run

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,12 @@
 #Using puppet to create a file with below specifications
 
-file {'school':
+file { 'school':
+    ensure => file,
+    path   => '/tmp/school',
+    mode   => '0744',
+    owner  => 'www-data',
+    group  => 'www-data',
+    content => 'I love Puppet'
 
+    
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+#Using puppet, install flask from pip3.
+#This manifest install Flask version 2.1.0 Using pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
